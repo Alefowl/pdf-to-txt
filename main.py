042ab77d61c925916e0425f18097ac7fb0dc84e5
Sererim <danielleymanw@gmail.com>
2024-07-01 12:02:09 +0300

    Added requirements.txt and pyproject and README

---
+++ main.py
@@ -0,0 +1,130 @@
+# Python std.
+from typing import List, Dict
+import argparse
+import re
+
+# Imported library.
+import fitz
+
+
+def replace_ligatures(ligatures : Dict[str, str], text : str, symbol : str) -> str:    
+    for search, replace in ligatures.items():
+        text = text.replace(search, replace)
+    return text
+
+
+def replace(text : str, numbers : bool, braces : bool) -> str:
+  if numbers:
+    text = re.sub(r'\d+', '', text)
+  if braces:
+    text = text.replace("[", "(").replace("]", ")")
+    text = text.replace("{", "(").replace("}", ")")
+  
+  return text
+
+
+def convert(filename : str, output : str, symbol : str, upper_limit : int,
+           low_cutoff : int, high_cutoff : int, nums : bool, braces : bool ) -> None:
+  
+  # Common ligatures present in latin alphabet.
+  ligatures : Dict[str, str] = {
+    "ﬀ"  : "ff",
+    "ﬁ"  : "fi",
+    "ﬂ"  : "fl",
+    "ﬃ"  : "ffi",
+    "ﬄ"  : "ffl",
+    "ﬅ"  : "ft",
+    "ﬆ"  : "st",
+    "Ꜳ" : "AA",
+    "Æ"  : "AE",
+    "ꜳ"  : "aa",
+    "\0" : symbol if symbol is not None else ""
+  }
+  
+  pdf_file = fitz.open(filename)
+  text  : str = ""
+  lines : List[str] = []
+    
+  with open(output, 'w+', encoding='utf-8') as file:
+    for page_num in range(low_cutoff,
+                          high_cutoff if high_cutoff is not None else pdf_file.page_count):
+      
+      lines = pdf_file[page_num].get_text().split('\n')
+      
+      for i, line in enumerate(lines):
+        # Chapter name probably.
+        if 1 < len(line) <= upper_limit and line[0].isupper():
+          text += f"{line}\n\n"
+        # Not a chapter name.
+        elif len(line) > 1:
+          # Start of a paragraph ?
+          if i + 1 < len(lines) and any(lines[i + 1]):
+            # Probably
+            if line.endswith(('.', '?', '!')) and lines[i + 1][0].isupper() and len(line) <= len(lines[i + 1]) - 6:
+              text += f"{line}\n\n"
+            # No
+            else:
+              text += f"{line}\n"
+          # No  
+          else:
+            text += f"{line}\n"
+        
+      if len(text) != 0:
+        # Replace ligatures with valid symbols.
+        text = replace_ligatures(ligatures, text, symbol)
+        # Delete numbers and braces from text.
+        text = replace(text, nums, braces)
+        # Write text to the file.
+        file.write(text)
+        text = ""
+  
+  pdf_file.close()
+  
+  return None
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(
+      prog="PDF to Text converter.",
+      description="Converts PDF file to valid TXT file for easier upload."
+  )
+  # Filename.
+  parser.add_argument('-filename', metavar='-F', nargs='?', 
+                      type=str, help="Path to a pdf file.",
+                      required=True)
+  # Output filename.
+  parser.add_argument('-output', metavar='-O', nargs='?',
+                      type=str, help='Name of the txt file.',
+                      default='output.txt')
+  # Errored symbol.
+  parser.add_argument('-symbol', metavar='-S', nargs='?',
+                      type=str, help="If program has troubles processing text.",)
+  # Upper limit.
+  parser.add_argument('-upper', metavar='-U', nargs='?',
+                      type=int, help="Upper limit of nummber of characters in chapter names.",
+                      default=25)
+  
+  # Low page cutoff.
+  parser.add_argument('-lcut', metavar='-L', nargs='?',
+                      type=int, help="Pages from the start of pdf that are not needed.")
+  
+  # High page cutoff.
+  parser.add_argument('-hcut', metavar='-H', nargs='?',
+                      type=int, help='Pages at the end of the pdf that are not needed.')
+  
+  # Deletes all the numbers.
+  parser.add_argument('-nums', metavar='-N', nargs='?',
+                      type=bool, help='Deletes all the numbers in the text.',
+                      default=False)
+  
+  # Replaces braces [], {} with ().
+  parser.add_argument('-braces', metavar='-B', nargs='?',
+                      type=bool, help="Replaces all [] with ().", default=False)
+  
+  args = parser.parse_args()
+  
+  convert(filename=args.filename, output=args.output, 
+          symbol=args.symbol, upper_limit=args.upper,
+          low_cutoff=args.lcut, high_cutoff=args.hcut,
+          nums=args.nums, braces=args.braces)
+  
