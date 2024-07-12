b1e298a96271a78f14f6fe4952fc34638acf2ffe
Sererim <danielleymanw@gmail.com>
2024-07-12 16:45:18 +0300

    Basic conversion works, no interactive mode :(

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ import fitz
 
 # Project files.
 from source.utils import ( set_fitz, get_pdf, close_pdf )
-from source.interactive import ( start, start_ocr, finish, finish_ocr )
 from source.automatic import ( convert, convert_ocr )
 
 
-def main(filename : str, output : str, symbol : str, upper_limit : int,
-           low_cutoff : int, high_cutoff : int, nums : bool, braces : bool,
-           ligatures : bool, align : str, dehy : bool, mode : bool, ocr : bool) -> None:
+def main(filename : str, output : str, upper_limit : int, low_cutoff : int, 
+    high_cutoff : int, nums : bool, braces : bool, ligatures : bool, 
+    align : str, dehy : bool, ocr : bool) -> None:
   
   # Settings for fitz.
   set_fitz(align=align, ligatures=ligatures, dehy=dehy)
   
   # Get pdf file.
-  pdf = get_pdf(filename=filename, ligatures=ligatures, align=align, dehy=dehy)
-  
-  if (mode):
-    start(pdf, output)
-  else:
-    convert(pdf_file=pdf, output=output, symbol=symbol, upper_limit=upper_limit, 
-            low_cutoff=low_cutoff, high_cutoff=high_cutoff, nums=nums, braces=braces)
+  try:
+    pdf = get_pdf(filename=filename, ligatures=ligatures, align=align, dehy=dehy)
+  except Exception as e:
+    print(f"Error: {e}")
+    return 1
+    
+  convert(pdf_file=pdf, output=output, upper_limit=upper_limit, 
+    low_cutoff=low_cutoff, high_cutoff=high_cutoff, nums=nums, braces=braces)
   
   # Close the pdf file.
   close_pdf(pdf)
@@ -38,14 +38,17 @@ if __name__ == '__main__':
       prog="PDF to Text converter.",
       description="Converts PDF file to valid TXT file for easier upload."
   )
+  
   # Filename.
   parser.add_argument('-filename', metavar='-F', nargs='?', 
                       type=str, help="Path to a pdf file.",
                       required=True)
+  
   # Output filename.
   parser.add_argument('-output', metavar='-O', nargs='?',
                       type=str, help='Name of the txt file.',
                       default='output.txt')
+  
   # Errored symbol.
   # parser.add_argument('-symbol', metavar='-S', nargs='?',
   #                     type=str, help="If program has troubles processing text.",)
@@ -88,11 +91,6 @@ if __name__ == '__main__':
                       type=bool, help='Ignore hyphens at line ends and join with next line', 
                       default=True)
   
-  # Use interactive mode.
-  parser.add_argument("-mode", metavar='-M', nargs='?', 
-                      type=bool, help='Use interactive mode.',
-                      default=False)
-  
   # OCR
   parser.add_argument('-ocr', metavar='-R', nargs='?',
                       type=bool, help='Use OCR for pdf.',
@@ -101,7 +99,7 @@ if __name__ == '__main__':
   args = parser.parse_args()
   
   main(filename=args.filename, output=args.output, 
-            symbol=args.symbol, upper_limit=args.upper,
-            low_cutoff=args.lcut, high_cutoff=args.hcut,
-            nums=args.nums, braces=args.braces, ligatures=args.ligatures,
-            align=args.align, dehy=args.dehy, mode=args.mode, ocr=args.ocr)
+      upper_limit=args.upper,
+      low_cutoff=args.lcut, high_cutoff=args.hcut,
+      nums=args.nums, braces=args.braces, ligatures=args.ligatures,
+      align=args.align, dehy=args.dehy, ocr=args.ocr)
--- source/automatic.py
+++ source/automatic.py
@@ -4,14 +4,22 @@ from typing import ( List )
 
 
 # None interactive mode.
-def convert(pdf_file, output : str, symbol : str, upper_limit : int,
-           low_cutoff : int, high_cutoff : int, nums : bool, braces : bool) -> None:
+def convert(pdf_file, output : str, upper_limit : int,
+  low_cutoff : int, high_cutoff : int, nums : bool, braces : bool) -> None:
+  """Function for automatic pdf to text conversion.
+  Args:
+      pdf_file (_type_): path to pdf file.
+      output (str): name of the output file.
+      upper_limit (int): upper limit for number of characters diff between 
+      the end of paragraph and beginning of a new one.
+      low_cutoff (int): pages from the beginning of the pdf file that shouldn't be converted.
+      high_cutoff (int): pages at the end of the pdf file that shouldn't be converted.
+      nums (bool): replace all numbers with empty spaces.
+      braces (bool): replace all {} and [] with ().
+  """
   
   text  : str = ""
   lines : List[str] = []
-  
-  # with open(output, 'w+', encoding='utf-8') as file:
-    
   for page_num in range(low_cutoff,
                         high_cutoff if high_cutoff is not None else pdf_file.page_count):
       
@@ -35,18 +43,27 @@ def convert(pdf_file, output : str, symbol : str, upper_limit : int,
           else:
             text += f"{line}\n"
         
-      if len(text) != 0:
-        # Delete numbers and braces from text.
-        text = replace(text, nums, braces)
-        # Write text to the file.
-        # file.write(text)
-        text = ""
-  
-  pdf_file.close()
+  if len(text) != 0:
+    # Delete numbers and braces from text.
+    text = replace(text, nums, braces)
+    with open(output, '+w', encoding='utf-8') as f:
+      f.write(text)
   
   return None
 
 
-def convert_ocr(pdf_file, output : str, symbol : str, upper_limit : int,
-           low_cutoff : int, high_cutoff : int, nums : bool, braces : bool) -> None:
+def convert_ocr(pdf_file, output : str, upper_limit : int,
+  low_cutoff : int, high_cutoff : int, nums : bool, braces : bool) -> None:
+  """Function for automatic OCR of pdf file into txt.
+  Args:
+      pdf_file (_type_): path to pdf file.
+      output (str): name of the output file.
+      upper_limit (int): upper limit for number of characters diff between 
+      the end of paragraph and beginning of a new one.
+      low_cutoff (int): pages from the beginning of the pdf file that shouldn't be converted.
+      high_cutoff (int): pages at the end of the pdf file that shouldn't be converted.
+      nums (bool): replace all numbers with empty spaces.
+      braces (bool): replace all {} and [] with ().
+  """
+  
   pass
